4b1170bb1469f7fa5b16ad05c0cb1c0d1b79877b
Sheridan <sheridanmaustad@gmail.com>
2021-04-12 18:33:02 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = (parameter1 = 2000, parameter2 = 100) => parameter1 / parameter2; 
+divide(2000,100);
+
+const square = (x => x ** 2);
+
+const add = (a= 3, b = 4) => a + b;
+add(3,4);
